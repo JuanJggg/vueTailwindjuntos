3ba79325eec0b7eca6d70720bfba688620eae717
SCIS3-PC08\SCIS3-08 <hividemi-8134@yopmail.com>
2023-06-05 13:22:01 -0500

    Vue con Tailwind.CSS

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import CardComponent from '@/components/CardComponent.vue';
+import { ref } from 'vue';
+
+const users = ref([
+    {
+        name:"Juana de arco",
+        correo: "juana@mail.com"
+    },
+    {
+        name:"Pepe",
+        correo: "pepe@mail.com"
+    }
+])
+
+const borrarTarjeta =(index) =>{
+    console.log("Kill to", index)
+    users.value.splice(index, 1)
+    console.log(users)
+}
+
+let name,correo;
+
+const adduser= ()=>{
+    const newUser = {
+        name: name,
+        correo: correo
+    }
+
+    users.value.unshift(newUser)
+}
+</script>
+
+<template>
+    <h1 class="text-2xl">
+        Hola Mundo
+    </h1>
+    <input type="text" v-model="name" placeholder="Esciba su nombre" class="border rounded">
+    <br><br>
+    <input type="text" v-model="correo" placeholder="Escriba su correo" class="border rounded">
+    <br>
+    <br>
+    <button class="bg-green-200 px-2 py-1 rounded" @click="adduser">Añadir</button>
+    <br>
+    <br>
+    <div class="space-y-2">
+        <CardComponent v-for="(user, index) in users" :key="index" :name="user.name" :correo="user.correo" :index="index" @borrarTarjeta="borrarTarjeta"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,43 @@
+<script setup>
+const mensaje=()=>{
+    alert("Diste clic en la foto");
+}
+
+const borrarTarjeta= (index) =>{
+    console.log("Borrar tajeta ",index)
+    emit("borrarTarjeta", index)
+}
+
+const emit = defineEmits(["borrarTarjeta"])
+
+const props = defineProps(
+    {
+        name: {
+            type: String,
+            default: "No name"
+        },
+        correo:{
+            type: String,
+            default: "No correo"
+        },
+        index:{
+            type: Number
+        }
+    }
+)
+</script>
+
+<template>
+    <div class="w-[230px] rounded shadow overflow-hidden">
+        <div class="inline-block align-top" @click="mensaje">
+            <img src="https://picsum.photos/40" alt="">
+        </div>
+        <div class="inline-block pl-2">
+            <h2 class="text-md font-semibold">{{ props.name }}</h2>
+            <p class="text-xs">{{ props.correo }}</p>
+        </div>
+        <span class="inline-block ml-4 bg-red-400 text-white rounded leading-0 py-1 px-2 hover:cursor-pointer hover:bg-red-600" @click="borrarTarjeta(props.index)">
+            X
+        </span>
+    </div>
+</template> 
